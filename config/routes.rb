1308894dd56b12a44322c0020f9c8daccc58493b
Volodymyr Kravchuk <volodymyr.kravchuk@mev.com>
2021-08-13 01:48:57 +0300

    Added Friendly URLs and Caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'people#index'
-  
-  resources :people, only: [:index]
+
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+
+  resources :people, only: [:index], concerns: :paginatable
 end
