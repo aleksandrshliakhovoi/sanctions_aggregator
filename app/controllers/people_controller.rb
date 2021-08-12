3f60afce0301c9f6132c686978cc816b9132c035
Volodymyr Kravchuk <volodymyr.kravchuk@mev.com>
2021-08-13 02:22:31 +0300

    Added search form

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,11 @@
 
 class PeopleController < ApplicationController
   def index
-    @people = Person.order(:last_name).order(:first_name).page params[:page]
+    if params[:search]
+      @people = Kaminari.paginate_array([], total_count: 0).page params[:page]
+      # TODO: Replace this dummy code by ES search
+    else
+      @people = Person.order(:last_name).order(:first_name).page params[:page]
+    end
   end
 end
