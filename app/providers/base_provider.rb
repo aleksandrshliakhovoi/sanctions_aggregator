466ad22178a63dc1377ac4a233db293aed7164ad
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2021-08-15 18:40:21 +0300

    try to fix errors with save objects

--- app/providers/base_provider.rb
+++ app/providers/base_provider.rb
@@ -3,17 +3,17 @@
 require "net/http"
 
 class BaseProvider
-  def save_to_db (validate_data)
+  def self.save_to_db (validate_data)
     Person.destroy_all
 
     validate_data.each do |person|
       Person.create(
-        first_name:         person.first_name,
-        last_name:          person.last_name,
-        citizenship:        person.citizenship,
-        birthday:           person.birthday,
-        source_base:        person.source_base,
-        end_sanctions_time: person.end_sanctions_time
+        first_name:         person["first_name"],
+        last_name:          person["last_name"],
+        citizenship:        person["citizenship"],
+        birthday:           person["birthday"],
+        source_base:        person["source_base"],
+        end_sanctions_time: person["end_sanctions_time"]
       )
     end
   end
--- app/providers/rnbo_provider.rb
+++ app/providers/rnbo_provider.rb
@@ -1,23 +1,46 @@
 # frozen_string_literal: true
 
 class RnboProvider < BaseProvider
+  PROVIDER_NAME = "РНБО"
+
   def receive_data(url)
-    uri = URI(url)
+    @file = Roo::Spreadsheet.open("./db/sanctions.xlsx")
+  end
 
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      request = Net::HTTP::Get.new uri
+  def validate_individuals
+    sanctions_individuals = receive_data("url").sheet("Фізичні особи")
+    sheet_header_arr = sanctions_individuals.row(1)
 
-      http.request request do |response|
-        open "sanctions", "w" do |io|
-          response.read_body do |chunk|
-            io.write chunk
-          end
-        end
-      end
+    if sheet_header_arr[8] ==      "Дата закінчення обмеження" &&
+       sheet_header_arr[9] ==      "Назва укр." &&
+       sheet_header_arr[10] ==     "Назва ориг." &&
+       sheet_header_arr[11] ==     "Назва альт." &&
+       sheet_header_arr[12] ==     "Дата народження" &&
+       sheet_header_arr[13] ==     "Громадянство"
+      sanctions_individuals.each do |row|
+         next if row[0] == "row"
+         sanction_individual_person = {
+           first_name:         row[9].split(" ")[1],
+           last_name:          row[9].split(" ")[0],
+           citizenship:        row[13],
+           birthday:           date_handler(row[12]),
+           source_base:        PROVIDER_NAME,
+           end_sanctions_time: date_handler(row[8])
+           }
+         pp sanction_individual_person
+         BaseProvider.save_to_db(sanction_individual_person)
+       end
+    else
+      puts "structure wrong"
     end
   end
 
-  def validate_source
-    puts validate
-  end
+  # def validate_entities
+  #   sanctions_entity = @file.sheet("Юридичні особи")
+  # end
+
+  private
+    def date_handler(row_value)
+      row_value != "" || nil ? row_value.to_date : nil
+    end
 end
