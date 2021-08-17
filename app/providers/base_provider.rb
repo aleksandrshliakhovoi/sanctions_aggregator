4f4724d91142d8e33c7e07b86add5b41378a0f7e
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2021-08-17 10:39:38 +0300

    Add individuals provider

--- app/providers/base_provider.rb
+++ app/providers/base_provider.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
-require "net/http"
-
 class BaseProvider
   def self.save_to_db (validate_data)
-    Person.destroy_all
-
-    validate_data.each do |person|
-      Person.create(
-        first_name:         person["first_name"],
-        last_name:          person["last_name"],
-        citizenship:        person["citizenship"],
-        birthday:           person["birthday"],
-        source_base:        person["source_base"],
-        end_sanctions_time: person["end_sanctions_time"]
-      )
+    validate_data.each do |item|
+      Person.create(item)
     end
   end
 end
--- app/providers/rnbo_provider.rb
+++ app/providers/rnbo_provider.rb
@@ -3,6 +3,8 @@
 class RnboProvider < BaseProvider
   PROVIDER_NAME = "РНБО"
 
+  @@rnbo_sanction_persons = []
+
   def receive_data(url)
     @file = Roo::Spreadsheet.open("./db/sanctions.xlsx")
   end
@@ -11,24 +13,42 @@ class RnboProvider < BaseProvider
     sanctions_individuals = receive_data("url").sheet("Фізичні особи")
     sheet_header_arr = sanctions_individuals.row(1)
 
-    if sheet_header_arr[8] ==      "Дата закінчення обмеження" &&
-       sheet_header_arr[9] ==      "Назва укр." &&
-       sheet_header_arr[10] ==     "Назва ориг." &&
-       sheet_header_arr[11] ==     "Назва альт." &&
-       sheet_header_arr[12] ==     "Дата народження" &&
-       sheet_header_arr[13] ==     "Громадянство"
+    sheet_header_column = {
+      end_date: sheet_header_arr[8],
+      ukr_name: sheet_header_arr[9],
+      orig_name: sheet_header_arr[10],
+      alter_name: sheet_header_arr[11],
+      birth_date: sheet_header_arr[12],
+      citezenship: sheet_header_arr[13]
+    }
+
+    if sheet_header_column[:end_date]    ==     "Дата закінчення обмеження" &&
+       sheet_header_column[:ukr_name]    ==     "Назва укр." &&
+       sheet_header_column[:orig_name]   ==     "Назва ориг." &&
+       sheet_header_column[:alter_name]  ==     "Назва альт." &&
+       sheet_header_column[:birth_date]  ==     "Дата народження" &&
+       sheet_header_column[:citezenship] ==     "Громадянство"
       sanctions_individuals.each do |row|
          next if row[0] == "row"
          sanction_individual_person = {
-           first_name:         row[9].split(" ")[1],
-           last_name:          row[9].split(" ")[0],
+           first_name:         [
+                                handle_name(row[9], 1),
+                                handle_name(row[9], 2),
+                                handle_name(row[10], 1),
+                                handle_name(row[10], 2),
+                                handle_name(row[11], 1)
+                               ],
+           last_name:          [
+                                handle_name(row[9], 0),
+                                handle_name(row[10], 0),
+                                handle_name(row[11], 0)
+                               ],
            citizenship:        row[13],
            birthday:           date_handler(row[12]),
            source_base:        PROVIDER_NAME,
            end_sanctions_time: date_handler(row[8])
            }
-         pp sanction_individual_person
-         BaseProvider.save_to_db(sanction_individual_person)
+         @@rnbo_sanction_persons << sanction_individual_person if sanction_individual_person.present?
        end
     else
       puts "structure wrong"
@@ -39,8 +59,16 @@ class RnboProvider < BaseProvider
   #   sanctions_entity = @file.sheet("Юридичні особи")
   # end
 
+  def save_persons
+    BaseProvider.save_to_db(@@rnbo_sanction_persons)
+  end
+
   private
     def date_handler(row_value)
-      row_value != "" || nil ? row_value.to_date : nil
+      row_value.to_datetime if row_value.present?
+    end
+
+    def handle_name(row_value, el_number)
+      row_value.split(" ")[el_number] if row_value.present?
     end
 end
