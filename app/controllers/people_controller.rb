db0112b7072b3757f5c91ef3efb00c6a47879ecd
Volodymyr Kravchuk <volodymyr.kravchuk@mev.com>
2021-08-18 10:55:42 +0300

    Code style fix

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,11 +6,11 @@ class PeopleController < ApplicationController
     search_by = params[:search] || "*"
     # provider_ids = params[:providers] || @providers.ids
     # TODO: Implement filtering by provider id
-    @people = Person.search search_by,
+    @people = Person.search(search_by,
                             fields: [:first_name, :last_name],
                             misspellings: false,
                             match: :text_middle,
                             load: false,
-                            page: params[:page]
+                            page: params[:page])
   end
 end
