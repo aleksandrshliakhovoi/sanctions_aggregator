4b634e941a60430cd1017852014185b54e7309ca
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2021-08-31 14:24:13 +0300

    add check persons method

--- app/providers/un_individual_provider.rb
+++ app/providers/un_individual_provider.rb
@@ -7,24 +7,33 @@ class UnIndividualProvider < BaseProvider
   def handle_data_from_xml
     un_individual_persons = []
 
-    individuals_dataset.each do |individual|
-      sanction_person = {
-        first_name:         individual.xpath('./FIRST_NAME').text,
-        last_name:          [
-                              individual.xpath('./SECOND_NAME').text,
-                              individual.xpath('./THIRD_NAME').text,
-                            ],
-        citizenship:        individual.xpath('./NATIONALITY').text,
-        birthday:           individual.xpath('./INDIVIDUAL_DATE_OF_BIRTH').text,
-        provider_id:        PROVIDER_ID,
-        end_sanctions_time: nil
-      }
-      un_individual_persons << sanction_person
+    if check_persons
+      individuals_dataset.each do |individual|
+        sanction_person = {
+          first_name:         individual.xpath('./FIRST_NAME').text,
+          last_name:          [
+                                individual.xpath('./SECOND_NAME').text,
+                                individual.xpath('./THIRD_NAME').text,
+                              ],
+          citizenship:        individual.xpath('./NATIONALITY').text,
+          birthday:           individual.xpath('./INDIVIDUAL_DATE_OF_BIRTH').text,
+          provider_id:        PROVIDER_ID,
+          end_sanctions_time: nil
+        }
+        un_individual_persons << sanction_person
+      end
     end
     un_individual_persons
   end
 
   private
+    def check_persons
+      find_all_first_name = receive_data.search('//INDIVIDUAL//FIRST_NAME').size
+      all_individual_persons = individuals_dataset.size
+
+      find_all_first_name == all_individual_persons ? true : false
+    end
+
     def receive_data
       Nokogiri::XML.parse(URI.open('https://scsanctions.un.org/resources/xml/en/consolidated.xml'))
     end
