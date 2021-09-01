54313fed77451febde3e6b7a8dd74d734db25f26
Oleksandr Shliakhovyi <aleksandrshliakhovoi@gmail.com>
2021-09-01 17:37:29 +0300

    Trying ti fix

---
+++ spec/providers/un_individual_provider_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe RnboProvider, tdd: true do
+
+  describe 'validate data handling method' do
+    
+    it 'should change size' do
+      
+    end
+
+    it 'should be an array' do
+      
+    end
+
+    it 'each element should have predefined structure' do
+      
+    end
+
+    
+  end
+
+end
